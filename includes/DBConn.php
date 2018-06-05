4a2f6d540c9fc62805622121c6e4229e9d5165f6
Tristan Constable <tristan.m.constable@gmail.com>
2018-06-05 11:13:16 +0200

    changed operating database to test

--- includes/DBConn.php
+++ includes/DBConn.php
@@ -11,7 +11,7 @@
 
 	function connect()
 	{
-		$myConn = new mysqli("127.0.0.1", "root", "", "wede_task_2");
+		$myConn = new mysqli("127.0.0.1", "root", "", "test");
 
 		if($myConn->connect_errno)
 		{
